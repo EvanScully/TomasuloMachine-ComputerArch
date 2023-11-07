b4169f4a01dd2523b6a54cd4795f310d8fdd62fb
Evan Scully <91391388+EvanScully@users.noreply.github.com>
2023-11-07 14:55:36 -0600

    Add files via upload
    The main .cpp file and the main .h file for the repo

---
+++ Tomasulo Machine.cpp
@@ -0,0 +1,12 @@
+﻿// Tomasulo Machine.cpp : Defines the entry point for the application.
+//
+
+#include "Tomasulo Machine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
