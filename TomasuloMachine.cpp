70502ce517f73cf9a414bc9b9ced49e941998acc
Evan Scully <evan.scully@ndsu.edu>
2023-11-07 23:09:42 -0600

    Added ASCII values for .cpp file

--- TomasuloMachine.cpp
+++ TomasuloMachine.cpp
@@ -5,6 +5,18 @@ int main()
 {
 	// Read Instruction File
 
+	/*	ASCII		Val
+		30			0
+		31			1
+		32			2
+		33			3
+		34			4
+		35			5
+		36			6
+		37			7
+		38			8
+		39			9
+	*/
 	std::ifstream myfile("Instructions.txt");
 	if (myfile.is_open()) {
 		char mychar;
